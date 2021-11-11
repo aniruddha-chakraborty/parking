af397e06e864659df811c3417a7988d8dab6c67a
Aniruddha Chakraborty <aniruddha@genex.digital>
2021-11-11 04:07:28 +0000

    initial

---
+++ app/Http/Controllers/ParkingController.php
@@ -0,0 +1,6 @@
+<?php
+
+
+class ParkingController extends BaseController {
+	
+}
---
+++ database/migrations/2021_11_10_173839_create_vehicle_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVehicleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicle', function (Blueprint $table) {
+            $table->id();
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicle');
+    }
+}
